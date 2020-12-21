8025fef7c7f55552dddefd17d530af88c38f483a
Huawei Chen <iam@huaweichen.com>
2020-12-21 23:03:22 +1000

    webpack

--- projects/2-7-webpack/src/index.js
+++ projects/2-7-webpack/src/index.js
@@ -1,3 +1,20 @@
 require('./index.scss')
+const happy = require('./happy.jpg')
+const logo = require('./logo.png')
 
 console.log('hello webpack!')
+
+function createImageElement(srcParam) {
+    let img = document.createElement("img");
+    img.src = srcParam
+    return img
+}
+
+const div = document.createElement('div')
+const happyImageElement = createImageElement(happy)
+const logoImageElement = createImageElement(logo)
+
+div.appendChild(happyImageElement)
+div.appendChild(logoImageElement)
+
+document.body.appendChild(div)
--- projects/2-7-webpack/webpack.config.js
+++ projects/2-7-webpack/webpack.config.js
@@ -1,22 +1,61 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    // path.join() 去拼接路径
-    // __dirname 当前文件的绝对路径
-    filename: 'bundle.js',
-    path: path.join(__dirname, './dist')
-  },
-  module: {
-    rules: [
-      {
-        // sass-loader node-sass两个依赖都需要安装
-        test: /\.(scss|sass)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
+    entry: './src/index.js',
+    output: {
+        // path.join() 去拼接路径
+        // __dirname 当前文件的绝对路径
+        filename: 'bundle.js',
+        path: path.join(__dirname, './dist-not-ignore')
+    },
+    devServer: {
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                // sass-loader node-sass两个依赖都需要安装
+                test: /\.(scss|sass)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                // https://webpack.js.org/loaders/url-loader/
+                test: /\.(png|jpg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 5 * 1024,
+                            esModule: false,
+                            outputPath: 'images',
+                            // https://webpack.js.org/loaders/file-loader/#hashtypehashdigesttypelength
+                            name: '[name]-[md4:hash:hex:4].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[md4:hash:hex:4].[ext]',
+                            outputPath: 'fonts',
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template.html'
+        })
     ]
-  }
 }
 
 module.exports = config

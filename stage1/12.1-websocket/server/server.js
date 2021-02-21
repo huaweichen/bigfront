3cedaed3522b5af5cafb3a04db789f3b5e4b9707
Huawei Chen <iam@huaweichen.com>
2021-02-21 12:20:04 +1000

    multi clients

--- stage1/12.1-websocket/server/server.js
+++ stage1/12.1-websocket/server/server.js
@@ -6,10 +6,14 @@ const wss = new WebSocket.Server({
 
 wss.on('connection', (ws) => {
     console.log('one client has been connected')
-
     ws.on('message', function (msg) {
         console.log('message from client to server: ' + msg)
-    })
 
-    ws.send('message from server to client')
+        // broadcast to each client apart from self
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN && ws !== client) {
+                client.send(msg);
+            }
+        });
+    })
 })
